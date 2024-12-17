26b734bc079afd1f8159331a8bb091d770796556
parkyuhyeon <pyh1111802@naver.com>
2024-12-18 08:48:40 +0900

    :memo: :: add main page

---
+++ lib/views/main/main_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+part './main_scaffold.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const _Scaffold();
+  }
+}
