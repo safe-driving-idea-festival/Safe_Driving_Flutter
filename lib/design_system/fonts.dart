7db3ba8c58bb09a7eed127eace9e63ab462fd359
parkyuhyeon <pyh1111802@naver.com>
2024-12-17 10:29:52 +0900

    💄 :: build font design system

---
+++ lib/design_system/fonts.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+
+class AppTypography {
+  static const String _fontFamily = "S-Core Dream";
+
+  static const title0 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  );
+  static const title1 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  );
+  static const title2B = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+  );
+  static const title2R = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w200,
+    fontFamily: _fontFamily,
+  );
+  static const title3B = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+  );
+  static const title3R = TextStyle(
+    fontSize: 20  ,
+    fontWeight: FontWeight.w200,
+    fontFamily: _fontFamily,
+  );
+  static const body1B = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+  );
+  static const body1R = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w200,
+    fontFamily: _fontFamily,
+  );
+  static const body2B = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+  );
+  static const body2R = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w200,
+    fontFamily: _fontFamily,
+  );
+  static const caption1B = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+  );
+  static const caption1R = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w200,
+    fontFamily: _fontFamily,
+  );
+  static const caption2B = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+  );
+  static const caption2R = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  );
+  static const caption3B = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w600,
+    fontFamily: _fontFamily,
+  );
+  static const caption3R = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w500,
+    fontFamily: _fontFamily,
+  );
+}
