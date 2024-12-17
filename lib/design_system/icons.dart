d9225fe6b09857c6725d5b77eb75abb7d0fb1dbd
parkyuhyeon <pyh1111802@naver.com>
2024-12-17 15:52:40 +0900

    :lipstick: :: build icon design system

--- lib/design_system/icons.dart
+++ lib/design_system/icons.dart
@@ -1,3 +1,49 @@
+import 'package:flutter_svg/flutter_svg.dart';
+
 class AppIcon{
-  
+  static const double width = 24;
+  static const double height = 24;
+
+  static SvgPicture bluetooth = SvgPicture.asset('assets/icons/bluetooth/bluetooth.svg', width: width, height: height,);
+  static SvgPicture bluetooth_disable = SvgPicture.asset('assets/icons/bluetooth/bluetooth disable.svg', width: width, height: height,);
+  static SvgPicture bluetooth_searching = SvgPicture.asset('assets/icons/bluetooth/bluetooth searching.svg', width: width, height: height,);
+  static SvgPicture bluetooth_no = SvgPicture.asset('assets/icons/bluetooth/no.svg', width: width, height: height,);
+  static SvgPicture black_car = SvgPicture.asset('assets/icons/car/black car.svg', width: width, height: height,);
+  static SvgPicture default_car = SvgPicture.asset('assets/icons/car/default car.svg', width: width, height: height,);
+  static SvgPicture light_car = SvgPicture.asset('assets/icons/car/light car.svg', width: width, height: height,);
+  static SvgPicture check = SvgPicture.asset('assets/icons/check/check.svg', width: width, height: height,);
+  static SvgPicture close = SvgPicture.asset('assets/icons/check/close.svg', width: width, height: height,);
+  static SvgPicture down_arrow = SvgPicture.asset('assets/icons/direction/down_arrow.svg', width: width, height: height,);
+  static SvgPicture forward_arrow = SvgPicture.asset('assets/icons/direction/forward_arrow.svg', width: width, height: height,);
+  static SvgPicture left_arrow = SvgPicture.asset('assets/icons/direction/left_arrow.svg', width: width, height: height,);
+  static SvgPicture right_arrow = SvgPicture.asset('assets/icons/direction/right_arrow.svg', width: width, height: height,);
+  static SvgPicture up_arrow = SvgPicture.asset('assets/icons/direction/up_arrow.svg', width: width, height: height,);
+  static SvgPicture back_arrow_ios = SvgPicture.asset('assets/icons/direction/back_arrow_ios.svg', width: width, height: height,);
+  static SvgPicture forward_arrow_ios = SvgPicture.asset('assets/icons/bluetooth/bluetooth.svg', width: width, height: height,);
+  static SvgPicture bar = SvgPicture.asset('assets/icons/etc/bar.svg', width: width, height: height,);
+  static SvgPicture email = SvgPicture.asset('assets/icons/etc/email.svg', width: width, height: height,);
+  static SvgPicture location_mark = SvgPicture.asset('assets/icons/etc/location mark.svg', width: width, height: height,);
+  static SvgPicture moon = SvgPicture.asset('assets/icons/etc/moon.svg', width: width, height: height,);
+  static SvgPicture speed = SvgPicture.asset('assets/icons/etc/speed.svg', width: width, height: height,);
+  static SvgPicture warning = SvgPicture.asset('assets/icons/etc/warning.svg', width: width, height: height,);
+  static SvgPicture password_on = SvgPicture.asset('assets/icons/password/on.svg', width: width, height: height,);
+  static SvgPicture password_off = SvgPicture.asset('assets/icons/password/off.svg', width: width, height: height,);
+  static SvgPicture phone_disable = SvgPicture.asset('assets/icons/phone/phone_disable.svg', width: width, height: height,);
+  static SvgPicture phone_enable = SvgPicture.asset('assets/icons/phone/phone_enable.svg', width: width, height: height,);
+  static SvgPicture play = SvgPicture.asset('assets/icons/play/play.svg', width: width, height: height,);
+  static SvgPicture play_disable = SvgPicture.asset('assets/icons/play/play_disable.svg', width: width, height: height,);
+  static SvgPicture volumn_off = SvgPicture.asset('assets/icons/sound/volumn_off.svg', width: width, height: height,);
+  static SvgPicture volumn_up = SvgPicture.asset('assets/icons/sound/volumn_up.svg', width: width, height: height,);
+  static SvgPicture tabbar_fill_alert = SvgPicture.asset('assets/icons/tabbar_fill/alert.svg', width: width, height: height,);
+  static SvgPicture tabbar_fill_home = SvgPicture.asset('assets/icons/tabbar_fill/home.svg', width: width, height: height,);
+  static SvgPicture tabbar_fill_music = SvgPicture.asset('assets/icons/tabbar_fill/music.svg', width: width, height: height,);
+  static SvgPicture tabbar_fill_profile = SvgPicture.asset('assets/icons/tabbar_fill/profile.svg', width: width, height: height,);
+  static SvgPicture tabbar_outline_alert = SvgPicture.asset('assets/icons/tabbar_outline/alert.svg', width: width, height: height,);
+  static SvgPicture tabbar_outline_home = SvgPicture.asset('assets/icons/tabbar_outline/home.svg', width: width, height: height,);
+  static SvgPicture tabbar_outline_music = SvgPicture.asset('assets/icons/tabbar_outline/music.svg', width: width, height: height,);
+  static SvgPicture tabbar_outline_profile = SvgPicture.asset('assets/icons/tabbar_outline/profile.svg', width: width, height: height,);
+  static SvgPicture battery = SvgPicture.asset('assets/icons/battery.svg', width: width, height: height,);
+  static SvgPicture group = SvgPicture.asset('assets/icons/group.svg', width: width, height: height,);
+  static SvgPicture seat = SvgPicture.asset('assets/icons/seat.svg', width: width, height: height,);
+  static SvgPicture logo = SvgPicture.asset('assets/image/logo.svg', width: 208, height: 67,);
 }
