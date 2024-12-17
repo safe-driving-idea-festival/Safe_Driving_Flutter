c700088ca282de2c82709848be50656599e28b42
parkyuhyeon <pyh1111802@naver.com>
2024-12-18 08:48:54 +0900

    :memo: :: add main scaffold

---
+++ lib/views/main/main_scaffold.dart
@@ -0,0 +1,14 @@
+part of './main_page.dart';
+
+class _Scaffold extends StatelessWidget {
+  const _Scaffold();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
