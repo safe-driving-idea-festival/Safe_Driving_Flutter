4021c9c4d2603f5f75f97ee9f571e2939061fb90
parkyuhyeon <pyh1111802@naver.com>
2024-12-17 22:20:40 +0900

    :lipstick: :: add login_page

---
+++ lib/views/login/login_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:safe_driving/design_system/colors.dart';
+import 'package:safe_driving/design_system/icons.dart';
+
+part './login_scaffold.dart';
+
+part './login_google_button.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const _Scaffold(
+      googleLogin: _GoogleLoginButton(),
+    );
+  }
+}
