2624aa81c9cb28f2fe3811daa799f5a1f43c147a
parkyuhyeon <pyh1111802@naver.com>
2024-12-18 17:47:31 +0900

    :lipstick: :: add home driving info

---
+++ lib/presentation/views/home/home_driving_info.dart
@@ -0,0 +1,10 @@
+part of 'home_page.dart';
+
+class _DrivingInfo extends StatelessWidget {
+  const _DrivingInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DrivingInfo();
+  }
+}
