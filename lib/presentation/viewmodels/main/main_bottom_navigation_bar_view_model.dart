ab2a4d0293e6c767d5090478eb20baebdd53b50b
parkyuhyeon <pyh1111802@naver.com>
2024-12-18 17:47:00 +0900

    :memo: :: update pageview to move smooth

--- lib/presentation/viewmodels/main/main_bottom_navigation_bar_view_model.dart
+++ lib/presentation/viewmodels/main/main_bottom_navigation_bar_view_model.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
-class MainBottomNavigationBarViewModel extends ChangeNotifier{
+class MainBottomNavigationBarViewModel extends ChangeNotifier {
   int _currentIndex = 0;
+  final PageController _pageController = PageController();
 
   int get currentIndex => _currentIndex;
+  PageController get pageController => _pageController;
 
-  void setIndex(int index){
+  void setIndex(int index) {
     _currentIndex = index;
+    _pageController.animateToPage(
+      _currentIndex,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
     notifyListeners();
   }
 }
