3f29a6112b4f2eac8e025b975c9d67fd2af890a0
parkyuhyeon <pyh1111802@naver.com>
2024-12-18 17:42:02 +0900

    :memo: :: add import and delete changenotifierprovider

--- lib/presentation/views/main/main_page.dart
+++ lib/presentation/views/main/main_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:safe_driving/presentation/viewmodels/main/main_bottom_navigation_bar_view_model.dart';
+import 'package:safe_driving/presentation/views/alert/alert_page.dart';
+import 'package:safe_driving/presentation/views/home/home_page.dart';
+import 'package:safe_driving/presentation/views/music/music_page.dart';
+import 'package:safe_driving/presentation/views/profile/profile_page.dart';
 import '../../../core/utils/colors.dart';
 import '../../../core/utils/icons.dart';
 
@@ -13,12 +17,9 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context)=>MainBottomNavigationBarViewModel(),
-      child: const _Scaffold(
-        appbar: _Appbar(),
-        bottomNavigationBar: _BottomNavigationBar(),
-      ),
+    return const _Scaffold(
+      appbar: _Appbar(),
+      bottomNavigationBar: _BottomNavigationBar(),
     );
   }
 }
