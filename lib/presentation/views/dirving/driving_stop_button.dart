5effdaf3772d68c9ce0ebbfde4f31006dc0b165f
parkyuhyeon <pyh1111802@naver.com>
2024-12-20 11:56:16 +0900

    :lipstick: :: add driving stop button

---
+++ lib/presentation/views/dirving/driving_stop_button.dart
@@ -0,0 +1,48 @@
+part of 'driving_page.dart';
+
+class _StopButton extends StatelessWidget {
+  const _StopButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return ButtonComponent(
+      width: double.infinity,
+      height: 48,
+      color: AppColor.main,
+      onTap:
+          context.read<DrivingViewModel>().drivingState == DrivingState.Pause
+              ? () {
+                  context.read<DrivingViewModel>().drivingOn();
+                  context.read<LocationViewModel>().initializeLocation();
+                }
+              : () => showDialog(
+                    context: context,
+                    builder: (builder) => const _StopModal(),
+                  ),
+      child: context.watch<DrivingViewModel>().drivingState ==
+              DrivingState.Pause
+          ? Row(
+              spacing: 8,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AppIcon.play(color: AppColor.white),
+                Text(
+                  '다시 운전하기',
+                  style: AppTypography.body1R.copyWith(color: AppColor.white),
+                ),
+              ],
+            )
+          : Row(
+              spacing: 8,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AppIcon.playDisable(color: AppColor.white),
+                Text(
+                  '잠시 멈추기',
+                  style: AppTypography.body1R.copyWith(color: AppColor.white),
+                ),
+              ],
+            ),
+    );
+  }
+}
