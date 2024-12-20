75e5340afc271d3bad2dc62d3a8465a2773c0e66
parkyuhyeon <pyh1111802@naver.com>
2024-12-20 11:58:24 +0900

    :lipstick: :: update splash page

--- lib/presentation/views/splash/splsah_page.dart
+++ lib/presentation/views/splash/splsah_page.dart
@@ -4,10 +4,18 @@ import '../../../core/utils/icons.dart';
 import '../login/login_page.dart';
 
 part 'splash_logo.dart';
+
 part 'splash_scaffold.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
 
   @override
   Widget build(BuildContext context) {
