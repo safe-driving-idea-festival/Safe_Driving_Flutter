9319e871405a908b8322c40c5cf37deee3ff3cd3
parkyuhyeon <pyh1111802@naver.com>
2024-12-20 11:59:15 +0900

    :lipstick: :: add sleep tag component

---
+++ lib/presentation/widgets/sleep_tag_component.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../../core/utils/colors.dart';
+import '../../core/utils/fonts.dart';
+import '../../core/utils/icons.dart';
+
+
+class SleepTagComponent extends StatelessWidget {
+  const SleepTagComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      width: 63,
+      height: 33,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: AppColor.error,
+          width: 1,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(4),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AppIcon.moon(
+            color: AppColor.error,
+            width: 16,
+            height: 16,
+          ),
+          Text(
+            '졸음',
+            style: AppTypography.caption1R.copyWith(
+              color: AppColor.error,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
