590b20f783d2058a95c3704304c3455ac908f73f
parkyuhyeon <pyh1111802@naver.com>
2024-12-20 11:55:19 +0900

    :lipstick: :: add driving modal

---
+++ lib/presentation/views/dirving/driving_drunk_modal.dart
@@ -0,0 +1,10 @@
+part of 'driving_page.dart';
+
+class _DrunkModal extends StatelessWidget {
+  const _DrunkModal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Dialog();
+  }
+}
