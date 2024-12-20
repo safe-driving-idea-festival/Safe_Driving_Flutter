4818acd1278f9af0d19592f01fe76a13d9edf749
parkyuhyeon <pyh1111802@naver.com>
2024-12-20 11:56:03 +0900

    :lipstick: :: add driving scaffold

---
+++ lib/presentation/views/dirving/driving_scaffold.dart
@@ -0,0 +1,38 @@
+part of 'driving_page.dart';
+
+class _Scaffold extends StatelessWidget {
+  const _Scaffold({
+    required this.appbar,
+    required this.drivingInfo,
+    required this.abnormalBehavior,
+    required this.stop,
+  });
+
+  final PreferredSizeWidget appbar;
+  final Widget drivingInfo;
+  final Widget abnormalBehavior;
+  final Widget stop;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appbar,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 24,
+        ),
+        child: Column(
+          children: [
+            drivingInfo,
+            const SizedBox(height: 30),
+            abnormalBehavior,
+            const Spacer(),
+            stop,
+            const SizedBox(height: 43,)
+          ],
+        ),
+      ),
+    );
+  }
+}
