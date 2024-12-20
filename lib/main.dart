cb5d3b1820d64c384f6eb61b28d0828d43aa442a
parkyuhyeon <pyh1111802@naver.com>
2024-12-20 11:59:26 +0900

    :memo: :: update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:safe_driving/presentation/viewmodels/main/bluetooth_view_model.dart';
+import 'package:safe_driving/presentation/viewmodels/driving/abnormal_behavior_view_model.dart';
+import 'package:safe_driving/presentation/viewmodels/driving/driving_view_model.dart';
+import 'package:safe_driving/presentation/viewmodels/driving/location_view_model.dart';
+import 'package:safe_driving/presentation/viewmodels/login/google_login_view_model.dart';
 import 'package:safe_driving/presentation/viewmodels/main/main_bottom_navigation_bar_view_model.dart';
 import 'package:safe_driving/presentation/views/splash/splsah_page.dart';
-
 import 'core/utils/colors.dart';
+import 'data/repositories/google_login_repository.dart';
+import 'data/repositories/location_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,11 +23,24 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          create: (BuildContext context) => BluetoothViewModel(),
+          create: (BuildContext context) => MainBottomNavigationBarViewModel(),
         ),
         ChangeNotifierProvider(
-          create: (BuildContext context) => MainBottomNavigationBarViewModel(),
-        )
+          create: (BuildContext context) => DrivingViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (BuildContext context) => AbnormalBehaviorViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (BuildContext context) => LocationViewModel(
+            GeolocatorLocationRepository(),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (BuildContext context) => GoogleLoginViewModel(
+            GoogleLoginRepositoryImpl(),
+          ),
+        ),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
