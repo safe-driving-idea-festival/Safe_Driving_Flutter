f0324828b150cbf3ab7034be7d47fa1425a9f06f
parkyuhyeon <pyh1111802@naver.com>
2024-12-20 11:55:51 +0900

    :lipstick: :: update driving page

--- lib/presentation/views/dirving/driving_page.dart
+++ lib/presentation/views/dirving/driving_page.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:safe_driving/core/utils/colors.dart';
+import 'package:safe_driving/core/utils/fonts.dart';
+import 'package:safe_driving/core/utils/icons.dart';
+import 'package:safe_driving/presentation/viewmodels/driving/abnormal_behavior_view_model.dart';
+import 'package:safe_driving/presentation/views/main/main_page.dart';
+import 'package:safe_driving/presentation/widgets/button_components.dart';
+import 'package:safe_driving/presentation/widgets/driving_info_component.dart';
+import 'package:safe_driving/presentation/widgets/drunk_tag_component.dart';
+import 'package:safe_driving/presentation/widgets/sleep_tag_component.dart';
+
+import '../../viewmodels/driving/driving_view_model.dart';
+import '../../viewmodels/driving/location_view_model.dart';
+
+part 'driving_scaffold.dart';
+part 'driving_info.dart';
+part 'driving_appbar.dart';
+part 'driving_abnormal_behavior.dart';
+part 'driving_stop_button.dart';
+part 'driving_stop_modal.dart';
+part 'driving_drunk_modal.dart';
 
 class DrivingPage extends StatelessWidget {
   const DrivingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const _Scaffold(
+      appbar: _Appbar(),
+      drivingInfo: _DrivingInfo(),
+      abnormalBehavior: _AbnormalBehavior(),
+      stop: _StopButton()
+    );
   }
 }
