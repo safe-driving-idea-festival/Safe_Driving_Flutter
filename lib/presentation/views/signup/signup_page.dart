3f5268302847df689b8414e9a76c1466b2c4429e
parkyuhyeon <pyh1111802@naver.com>
2024-12-23 15:25:33 +0900

    :memo: :: add signup page

---
+++ lib/presentation/views/signup/signup_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:safe_driving/core/utils/icons.dart';
+import 'package:safe_driving/presentation/viewmodels/login/login_view_model.dart';
+import 'package:safe_driving/presentation/views/main/main_page.dart';
+import 'package:safe_driving/presentation/widgets/textfield_component.dart';
+import '../../../core/utils/colors.dart';
+import '../../../core/utils/fonts.dart';
+import '../../widgets/button_components.dart';
+
+part 'signup_scaffold.dart';
+
+part 'signup_appbar.dart';
+
+part 'signup_header.dart';
+
+part 'signup_name.dart';
+
+part 'signup_camId.dart';
+
+part 'signup_emergency_number.dart';
+
+part 'signup_button.dart';
+
+class SignupPage extends StatelessWidget {
+  SignupPage({super.key});
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return _Scaffold(
+      appbar: const _Appbar(),
+      header: const _Header(),
+      name: const _Name(),
+      camId: const _CamId(),
+      emergencyNumbers: _EmergencyNumber(),
+      button: _Button(
+        formKey: formKey,
+      ),
+      formKey: formKey,
+    );
+  }
+}
