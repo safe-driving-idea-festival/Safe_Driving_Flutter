605f30e2921171e54c64ef921be5a38c0307dcad
parkyuhyeon <pyh1111802@naver.com>
2024-12-23 15:24:24 +0900

    :memo: :: add signup camid

---
+++ lib/presentation/views/signup/signup_camId.dart
@@ -0,0 +1,37 @@
+part of 'signup_page.dart';
+
+class _CamId extends StatelessWidget {
+  const _CamId({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 8,
+      children: [
+        Row(
+          children: [
+            const Text(
+              '카메라 아이디',
+              style: AppTypography.body2R,
+            ),
+            Text(
+              '*',
+              style: AppTypography.body2R.copyWith(color: AppColor.error),
+            ),
+          ],
+        ),
+        TextfieldComponent(
+          hintText: '카메라 아이디를 입력해주세요',
+          controller: context.read<AuthViewModel>().camIdController,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return '카메라 아이디를 입력해주세요';
+            }
+            return null;
+          },
+        ),
+      ],
+    );
+  }
+}
