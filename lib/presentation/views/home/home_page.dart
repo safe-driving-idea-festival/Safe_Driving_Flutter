66ad635cfa09130af31dccc135c054e2bb679ea1
parkyuhyeon <pyh1111802@naver.com>
2024-12-23 15:23:05 +0900

    :memo: :: update home_page

--- lib/presentation/views/home/home_page.dart
+++ lib/presentation/views/home/home_page.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:safe_driving/core/utils/colors.dart';
@@ -8,14 +7,11 @@ import 'package:safe_driving/presentation/viewmodels/driving/location_view_model
 import 'package:safe_driving/presentation/views/dirving/driving_page.dart';
 import 'package:safe_driving/presentation/widgets/button_components.dart';
 import 'package:safe_driving/presentation/widgets/driving_info_component.dart';
-
-import '../../../core/utils/icons.dart';
 import '../../viewmodels/driving/abnormal_behavior_view_model.dart';
 
 part 'home_scaffold.dart';
 part 'home_driving_info.dart';
 part 'home_start_button.dart';
-part 'home_bluetooth_connect_modal.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
