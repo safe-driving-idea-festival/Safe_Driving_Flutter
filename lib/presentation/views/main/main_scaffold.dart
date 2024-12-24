1c7f13c8cb54e3508cd5c768f45189a1e4ad5f85
parkyuhyeon <pyh1111802@naver.com>
2024-12-25 01:06:16 +0900

    :memo: :: update main scaffold

--- lib/presentation/views/main/main_scaffold.dart
+++ lib/presentation/views/main/main_scaffold.dart
@@ -2,30 +2,19 @@ part of 'main_page.dart';
 
 class _Scaffold extends StatelessWidget {
   const _Scaffold({
-    required this.appbar,
     required this.bottomNavigationBar,
   });
 
-  final PreferredSizeWidget appbar;
   final Widget bottomNavigationBar;
   static const List<Widget> body = [
     HomePage(),
-    MusicPage(),
-    AlertPage(),
     ProfilePage()
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appbar,
-      body: PageView(
-        controller:
-            context.read<MainBottomNavigationBarViewModel>().pageController,
-        onPageChanged: (index) =>
-            context.read<MainBottomNavigationBarViewModel>().setIndex(index),
-        children: body,
-      ),
+  body: body[context.watch<MainBottomNavigationBarViewModel>().currentIndex],
       bottomNavigationBar: bottomNavigationBar,
     );
   }
