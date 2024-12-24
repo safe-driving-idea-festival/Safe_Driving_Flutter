53b77462743ac3f042ad3bf407933e8c8408b05b
parkyuhyeon <pyh1111802@naver.com>
2024-12-25 01:01:09 +0900

    :memo: :: update main bottom navigation bar view model

--- lib/presentation/viewmodels/main/main_bottom_navigation_bar_view_model.dart
+++ lib/presentation/viewmodels/main/main_bottom_navigation_bar_view_model.dart
@@ -2,20 +2,12 @@ import 'package:flutter/material.dart';
 
 class MainBottomNavigationBarViewModel extends ChangeNotifier {
   int _currentIndex = 0;
-  final PageController _pageController = PageController();
 
   int get currentIndex => _currentIndex;
 
-  PageController get pageController => _pageController;
-
   void setIndex(int index) {
     _currentIndex = index;
-    notifyListeners();
-    _pageController.animateToPage(
-      _currentIndex,
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
-    );
+    print(_currentIndex);
     notifyListeners();
   }
 }
