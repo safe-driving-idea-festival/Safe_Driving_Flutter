81accd6db5b59fc14daebdfe2bd3358e7ad51ffa
parkyuhyeon <pyh1111802@naver.com>
2024-12-25 01:02:31 +0900

    :memo: :: update home page

--- lib/presentation/views/home/home_page.dart
+++ lib/presentation/views/home/home_page.dart
@@ -4,14 +4,17 @@ import 'package:safe_driving/core/utils/colors.dart';
 import 'package:safe_driving/core/utils/fonts.dart';
 import 'package:safe_driving/presentation/viewmodels/driving/driving_view_model.dart';
 import 'package:safe_driving/presentation/viewmodels/driving/location_view_model.dart';
+import 'package:safe_driving/presentation/viewmodels/login/login_view_model.dart';
 import 'package:safe_driving/presentation/views/dirving/driving_page.dart';
 import 'package:safe_driving/presentation/widgets/button_components.dart';
 import 'package:safe_driving/presentation/widgets/driving_info_component.dart';
+import '../../../core/utils/icons.dart';
 import '../../viewmodels/driving/abnormal_behavior_view_model.dart';
 
 part 'home_scaffold.dart';
 part 'home_driving_info.dart';
 part 'home_start_button.dart';
+part 'home_appbar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -19,6 +22,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const _Scaffold(
+      appbar: _Appbar(),
       drivingInfo: _DrivingInfo(),
       button: _StartButton(),
     );
