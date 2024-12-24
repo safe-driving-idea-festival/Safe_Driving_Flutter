c4349606476efc5e0b01beed206a1b3a556652a3
parkyuhyeon <pyh1111802@naver.com>
2024-12-25 01:02:39 +0900

    :memo: :: update home scaffold

--- lib/presentation/views/home/home_scaffold.dart
+++ lib/presentation/views/home/home_scaffold.dart
@@ -4,14 +4,17 @@ class _Scaffold extends StatelessWidget {
   const _Scaffold({
     required this.drivingInfo,
     required this.button,
+    required this.appbar,
   });
 
+  final PreferredSizeWidget appbar;
   final Widget drivingInfo;
   final Widget button;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: appbar,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
