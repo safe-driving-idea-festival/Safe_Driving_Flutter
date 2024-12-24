d6d99addbc2114f316837d2498b5e7fca9b9d318
parkyuhyeon <pyh1111802@naver.com>
2024-12-25 01:08:56 +0900

    :memo: :: update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 import 'package:safe_driving/data/repositories/auth_repository.dart';
+import 'package:safe_driving/data/repositories/drive_repository.dart';
+import 'package:safe_driving/data/repositories/profile_repository.dart';
 import 'package:safe_driving/presentation/viewmodels/driving/abnormal_behavior_view_model.dart';
 import 'package:safe_driving/presentation/viewmodels/driving/driving_view_model.dart';
 import 'package:safe_driving/presentation/viewmodels/driving/location_view_model.dart';
 import 'package:safe_driving/presentation/viewmodels/login/login_view_model.dart';
 import 'package:safe_driving/presentation/viewmodels/main/main_bottom_navigation_bar_view_model.dart';
+import 'package:safe_driving/presentation/viewmodels/profile/profile_view_model.dart';
 import 'package:safe_driving/presentation/views/splash/splsah_page.dart';
 import 'core/utils/colors.dart';
 import 'data/repositories/google_login_repository.dart';
@@ -14,6 +18,7 @@ import 'data/repositories/location_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  GoogleSignIn().signOut();
   await dotenv.load(fileName: '.env');
   // await Firebase.initializeApp();
   // FirebaseMessaging messaging = FirebaseMessaging.instance;
@@ -33,7 +38,9 @@ class MyApp extends StatelessWidget {
           create: (BuildContext context) => MainBottomNavigationBarViewModel(),
         ),
         ChangeNotifierProvider(
-          create: (BuildContext context) => DrivingViewModel(),
+          create: (BuildContext context) => DrivingViewModel(
+            DriveRepositoryImpl(),
+          ),
         ),
         ChangeNotifierProvider(
           create: (BuildContext context) => AbnormalBehaviorViewModel(),
@@ -47,10 +54,13 @@ class MyApp extends StatelessWidget {
           create: (BuildContext context) => AuthViewModel(
             GoogleLoginRepositoryImpl(),
             AuthRepositoryImpl(),
-
           ),
         ),
-
+        ChangeNotifierProvider(
+          create: (BuildContext context) => ProfileViewModel(
+            ProfileRepositoryImpl(),
+          ),
+        )
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
