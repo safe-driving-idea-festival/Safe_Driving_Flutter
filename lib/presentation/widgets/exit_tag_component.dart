97677914333ca5b38dc21bf393b5834337688724
parkyuhyeon <pyh1111802@naver.com>
2024-12-25 01:08:17 +0900

    :memo: :: add exit tag component

---
+++ lib/presentation/widgets/exit_tag_component.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../../core/utils/colors.dart';
+import '../../core/utils/fonts.dart';
+import '../../core/utils/icons.dart';
+
+
+class ExitTagComponent extends StatelessWidget {
+  const ExitTagComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height: 33,
+      width: 94,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: AppColor.error,
+          width: 1,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(4),
+        ),
+      ),
+      child: Row(
+        spacing: 4,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AppIcon.bar(
+            color: AppColor.error,
+            width: 16,
+            height: 16,
+          ),
+          Text(
+            '화면 이탈',
+            style: AppTypography.caption1R.copyWith(
+              color: AppColor.error,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
