bf90dad1bd70a0b3d078045932490ab80f4b24ed
parkyuhyeon <pyh1111802@naver.com>
2024-12-25 01:03:15 +0900

    :memo: :: update main bottom navigation bar

--- lib/presentation/views/main/main_bottom_navigation_bar.dart
+++ lib/presentation/views/main/main_bottom_navigation_bar.dart
@@ -30,7 +30,6 @@ class _BottomNavigationBar extends StatelessWidget {
               .watch<MainBottomNavigationBarViewModel>()
               .currentIndex ==
               0
-
               ? AppIcon.tabbarFillHome(color: AppColor.main)
               : AppIcon.tabbarOutlineHome(color: AppColor.gray500),
           label: '홈',
@@ -41,26 +40,6 @@ class _BottomNavigationBar extends StatelessWidget {
               .watch<MainBottomNavigationBarViewModel>()
               .currentIndex ==
               1
-              ? AppIcon.tabbarFillMusic(color: AppColor.main)
-              : AppIcon.tabbarOutlineMusic(color: AppColor.gray500),
-          label: '음악',
-        ),
-        BottomNavigationBarItem(
-          icon:
-          context
-              .watch<MainBottomNavigationBarViewModel>()
-              .currentIndex ==
-              2
-              ? AppIcon.tabbarFillAlert(color: AppColor.main)
-              : AppIcon.tabbarOutlineAlert(color: AppColor.gray500),
-          label: '알림',
-        ),
-        BottomNavigationBarItem(
-          icon:
-          context
-              .watch<MainBottomNavigationBarViewModel>()
-              .currentIndex ==
-              3
               ? AppIcon.tabbarFillProfile(color: AppColor.main)
               : AppIcon.tabbarOutlineProfile(color: AppColor.gray500),
           label: '프로필',
