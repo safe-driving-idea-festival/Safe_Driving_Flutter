871df058dfdddc063e2e8cd1f24523eed4ca5d7e
parkyuhyeon <pyh1111802@naver.com>
2024-12-25 01:00:36 +0900

    :memo: :: update driving view model

--- lib/presentation/viewmodels/driving/driving_view_model.dart
+++ lib/presentation/viewmodels/driving/driving_view_model.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:safe_driving/data/models/auth_model.dart';
+import 'package:safe_driving/data/models/drive_model.dart';
+import 'package:safe_driving/data/repositories/drive_repository.dart';
 
 enum DrivingState {
   on,
@@ -8,20 +11,40 @@ enum DrivingState {
 
 class DrivingViewModel extends ChangeNotifier {
   DrivingState _drivingState = DrivingState.off;
+  final DriveRepository _driveRepository;
+  DriveStopModel? _driveStopModel;
+  bool _isLoading = false;
+
+  DrivingViewModel(this._driveRepository);
 
   DrivingState get drivingState => _drivingState;
 
-  void drivingOn() {
+  DriveRepository get driveRepository => _driveRepository;
+
+  DriveStopModel? get driveStopModel => _driveStopModel;
+
+  bool get isLoading => _isLoading;
+
+  void drivingOn(LoginResponseModel loginResponseModel) async{
+    _isLoading = true;
+    notifyListeners();
+   await driveRepository.start(loginResponseModel);
     _drivingState = DrivingState.on;
+    _isLoading = false;
     notifyListeners();
   }
 
-  void drivingOff() {
+  void drivingOff(LoginResponseModel loginResponseModel) async {
+    _isLoading = true;
+    notifyListeners();
+    _driveStopModel = await driveRepository.stop(loginResponseModel);
     _drivingState = DrivingState.off;
+    _isLoading = false;
     notifyListeners();
   }
 
   void drivingPause() {
+
     _drivingState = DrivingState.pause;
     notifyListeners();
   }
