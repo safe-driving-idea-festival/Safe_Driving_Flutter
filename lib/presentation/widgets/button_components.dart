e7b3fbbede9fe7779f41563d1817e6057f1f60c7
parkyuhyeon <pyh1111802@naver.com>
2024-12-29 03:27:35 +0900

    :memo: :: change button_components.dart

--- lib/presentation/widgets/button_components.dart
+++ lib/presentation/widgets/button_components.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart'; // ScreenUtil 추가
 
 class ButtonComponent extends StatelessWidget {
   const ButtonComponent({
@@ -19,16 +20,16 @@ class ButtonComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      borderRadius: const BorderRadius.all(
-        Radius.circular(4),
+      borderRadius: BorderRadius.all(
+        Radius.circular(4.r), // .r 추가
       ),
       color: color,
       child: InkWell(
         onTap: onTap,
         child: Container(
           alignment: Alignment.center,
-          width: width,
-          height: height,
+          width: width.w, // .w 추가
+          height: height.h, // .h 추가
           child: child,
         ),
       ),
