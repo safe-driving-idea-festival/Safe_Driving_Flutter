6cd66b53f4882f594d52eebe72e63a604275a940
parkyuhyeon <pyh1111802@naver.com>
2024-12-29 03:27:23 +0900

    :memo: :: change almost sleep tag components.dart

--- lib/presentation/widgets/almost_sleep_tag_component.dart
+++ lib/presentation/widgets/almost_sleep_tag_component.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart'; // ScreenUtil 추가
 
 import '../../core/utils/colors.dart';
 import '../../core/utils/fonts.dart';
@@ -11,32 +12,31 @@ class AlmostSleepTagComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      width: 63,
-      height: 33,
+      width: 63.w, // .w 추가
+      height: 33.h, // .h 추가
       decoration: BoxDecoration(
         border: Border.all(
           color: AppColor.error,
-          width: 1,
+          width: 1.w, // .w 추가
         ),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(4),
+        borderRadius: BorderRadius.all(
+          Radius.circular(4.r), // .r 추가
         ),
       ),
       child: Row(
-        spacing: 4,
+        spacing: 4.w,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           AppIcon.bar(
             color: AppColor.error,
-            width: 16,
-            height: 16,
-          ),
-          Text(
+            width: 16.w, // .w 추가
+            height: 16.h, // .h 추가
+          ), Text(
             '졸음',
             style: AppTypography.caption1R.copyWith(
               color: AppColor.error,
             ),
-          )
+          ),
         ],
       ),
     );
