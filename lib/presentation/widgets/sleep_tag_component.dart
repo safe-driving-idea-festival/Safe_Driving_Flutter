ae89dbe8406b5231058b9aa22458bb02b16ad83d
parkyuhyeon <pyh1111802@naver.com>
2024-12-29 03:28:12 +0900

    :memo: :: change sleep_tag_component.dart

--- lib/presentation/widgets/sleep_tag_component.dart
+++ lib/presentation/widgets/sleep_tag_component.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart'; // .w, .h 사용을 위해 추가
 
 import '../../core/utils/colors.dart';
 import '../../core/utils/fonts.dart';
 import '../../core/utils/icons.dart';
-
 
 class SleepTagComponent extends StatelessWidget {
   const SleepTagComponent({super.key});
@@ -12,26 +12,25 @@ class SleepTagComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      width: 63,
-      height: 33,
+      width: 63.w, // 수정: .w 추가
+      height: 33.h, // 수정: .h 추가
       decoration: BoxDecoration(
         border: Border.all(
           color: AppColor.error,
-          width: 1,
+          width: 1.w, // 수정: .w 추가
         ),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(4),
+        borderRadius: BorderRadius.all(
+          Radius.circular(4.r), // 수정: .r 추가
         ),
       ),
       child: Row(
-        spacing: 4,
-
+        spacing: 4.w,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           AppIcon.moon(
             color: AppColor.error,
-            width: 16,
-            height: 16,
+            width: 16.w, // 수정: .w 추가
+            height: 16.h, // 수정: .h 추가
           ),
           Text(
             '수면',
