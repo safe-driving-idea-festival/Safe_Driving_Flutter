e3abbb8e3bc47ed660ca53bfdcde987e576fdfd0
parkyuhyeon <pyh1111802@naver.com>
2024-12-29 03:28:02 +0900

    :memo: :: change exit_tag_component.dart

--- lib/presentation/widgets/exit_tag_component.dart
+++ lib/presentation/widgets/exit_tag_component.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart'; // .w, .h 사용을 위해 추가
 
 import '../../core/utils/colors.dart';
 import '../../core/utils/fonts.dart';
 import '../../core/utils/icons.dart';
-
 
 class ExitTagComponent extends StatelessWidget {
   const ExitTagComponent({super.key});
@@ -12,32 +12,32 @@ class ExitTagComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      height: 33,
-      width: 94,
+      height: 33.h, // 수정: .h 추가
+      width: 94.w, // 수정: .w 추가
       decoration: BoxDecoration(
         border: Border.all(
           color: AppColor.error,
-          width: 1,
+          width: 1.w, // 수정: .w 추가
         ),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(4),
+        borderRadius: BorderRadius.all(
+          Radius.circular(4.r), // 수정: .r 추가
         ),
       ),
       child: Row(
-        spacing: 4,
+        spacing: 4.w,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           AppIcon.bar(
             color: AppColor.error,
-            width: 16,
-            height: 16,
+            width: 16.w, // 수정: .w 추가
+            height: 16.h, // 수정: .h 추가
           ),
           Text(
             '화면 이탈',
             style: AppTypography.caption1R.copyWith(
               color: AppColor.error,
             ),
-          )
+          ),
         ],
       ),
     );
