ec88baee04eeab116567860b3efc00e0cf4123c8
parkyuhyeon <pyh1111802@naver.com>
2024-12-29 03:24:30 +0900

    :memo: :: change profile_drive_detection.dart

--- lib/presentation/views/profile/profile_drive_detection.dart
+++ lib/presentation/views/profile/profile_drive_detection.dart
@@ -11,20 +11,20 @@ class _DriveDetection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      padding: const EdgeInsets.symmetric(
-        horizontal: 24,
-        vertical: 16,
+      padding: EdgeInsets.symmetric(
+        horizontal: 24.w,
+        vertical: 16.h,
       ),
       decoration: BoxDecoration(
         border: Border.all(
           color: AppColor.gray100,
         ),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(4),
+        borderRadius: BorderRadius.all(
+          Radius.circular(4.r),
         ),
       ),
       child: Column(
-        spacing: 12,
+        spacing: 12.h,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
@@ -32,28 +32,33 @@ class _DriveDetection extends StatelessWidget {
             style: AppTypography.title2B,
           ),
           driveModel.detectionCount == 0
-              ? const Text(
+              ? Text(
                   '발견된 이상행동이 없습니다.',
                   style: AppTypography.caption1R,
                 )
               : Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 5.h,
                   children: [
                     Wrap(
-                      spacing: 4,
+                      spacing: 4.w,
+                      runSpacing: 5.h,
                       children: List.generate(
                         driveModel.detectionInfo.sleeping,
                         (_) => const SleepTagComponent(),
                       ),
                     ),
                     Wrap(
-                      spacing: 4,
+                      spacing: 4.w,
+                      runSpacing: 5.h,
                       children: List.generate(
                         driveModel.detectionInfo.almostSleep,
                         (_) => const AlmostSleepTagComponent(),
                       ),
                     ),
                     Wrap(
-                      spacing: 4,
+                      spacing: 4.w,
+                      runSpacing: 5.h,
                       children: List.generate(
                         driveModel.detectionInfo.exit,
                         (_) => const ExitTagComponent(),
